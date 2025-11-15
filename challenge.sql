cf0d7dc7f1ba33595a0a1ac45062e5a27cdf6cb4
Nikita Siniayev <ns21@usf.edu>
2025-11-15 13:22:56 -0500

    task 1

---
+++ challenge.sql
@@ -0,0 +1,13 @@
+--TASK 1
+
+SELECT
+  c.first_name || ' ' || c.last_name AS customer_name,
+  SUM(oi.quantity * oi.unit_price) AS total_spend
+FROM customers c
+JOIN orders o
+  ON o.customer_id = c.id
+JOIN order_items oi
+  ON oi.order_id = o.id
+GROUP BY c.id
+ORDER BY total_spend DESC
+LIMIT 5;
